16195aac586350e81be30002609aab17433c0665
anrocks <ashishnimrot1290@gmail.com>
2024-06-03 00:00:55 +0530

    Added routes

--- src/AppConfig.tsx
+++ src/AppConfig.tsx
@@ -1,15 +1,31 @@
 import { ThemeProvider, Typography } from "@mui/material";
 import theme from "./theme/Theme";
 import { AxiosHandler } from "./api/axios";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/dashboard/Dashboard";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
 export const AppConfig = () => {
   return (
     <ThemeProvider theme={theme}>
       <AxiosHandler>
         <Typography variant="h1" color="primary">
-          GPaaS
+          <GetRoutes />
         </Typography>
       </AxiosHandler>
     </ThemeProvider>
+  );
+};
+
+export const GetRoutes = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
   );
 };
