347ebba01307769f2e34f882158851a6deddb408
anrocks <ashishnimrot1290@gmail.com>
2024-06-09 15:50:56 +0530

    Added atoms

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
+import { RecoilRoot } from 'recoil'
 import './App.css'
 import { AppConfig } from './AppConfig'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { RecoilDevtools } from 'recoil-devtools'
 
 const queryClient = new QueryClient()
+
+// const isDevelopment = vite.env.NODE_ENV === 'development'
+// let RecoilDevTools = null
+// if (isDevelopment) {
+//   RecoilDevTools = require('recoil-devtools').RecoilDevTools
+// }
+// {isDevelopment && <RecoilDevTools />}
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <AppConfig />
+      <RecoilRoot>
+        <RecoilDevtools />
+        <AppConfig />
+      </RecoilRoot>
       <ReactQueryDevtools position="right" initialIsOpen={false} />
     </QueryClientProvider>
   )
