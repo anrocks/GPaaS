ff698de9ababd8700ac40efa5abf9499d4311cfe
Vaishnavi Nimrot <152794017+vaishnavinimrot@users.noreply.github.com>
2024-06-10 15:54:30 +0530

    Add register page

---
+++ src/api/models/Register.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+export const RegisterForm = z.object({
+  name: z.string().min(3, "Name must be at least 3 characters"),
+  email: z.string().email("Invalid email format"),
+  password: z.string().min(6, "Password must be at least 6 characters"),
+  repeatpassword: z.string().min(6, "Repeat Password must be at least 6 characters")
+    })
+    .refine((data) => data.repeatpassword === data.password, {
+        message: "Passwords don't match",
+        path: ["repeatpassword"],
+});
+
+export type dRegisterForm = z.infer<typeof RegisterForm>;
