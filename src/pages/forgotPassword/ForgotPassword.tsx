568c94f227f58eca4013b0ff396a7865eabc6a58
anrocks <ashishnimrot1290@gmail.com>
2024-06-11 23:42:33 +0530

    Create ForgotPassword.tsx

--- src/pages/forgotPassword/ForgotPassword.tsx
+++ src/pages/forgotPassword/ForgotPassword.tsx
@@ -1,69 +1,41 @@
-import { useState, } from "react";
+import { useCallback } from "react";
 import { Link } from "react-router-dom";
 import { TextField, Button, Typography, Container, } from "@mui/material";
-import {  z } from "zod";
+import * as style from './forgotpassword.style'
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm,Controller } from "react-hook-form";
 import { useSnackbar } from 'notistack'
-
-const schema = z.object({
-  email: z.string().email("Invalid email format"),
-});
-type FormData = z.infer<typeof schema>;
-
+import { IForgotPasswordForm, ForgotPasswordForm } from '../../api/models/IForgotPassword'
+import { forgotpasswordPost } from '../../api/endpoints/forgotpassword'
 const ForgotPassword = () => {
- 
-   
-    const [message, setMessage] = useState<string>("");
-   const [error, setError] = useState<string>("");
-   const { enqueueSnackbar } = useSnackbar()
+ const { enqueueSnackbar } = useSnackbar()
    const{
   control,
     handleSubmit,
-  } = useForm<FormData>({
-    resolver: zodResolver(schema),
+  } = useForm<IForgotPasswordForm>({
+    resolver: zodResolver(ForgotPasswordForm),
   });
  
-  const onSubmit = async (data: FormData) => {
+  const onSubmit = useCallback (
+    async (data: IForgotPasswordForm) => {
     try {
-      // Send a request to your backend to initiate password reset
-      const response = await fetch("/api/forgot-password", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      });
-
-      if (response.ok) {
-        throw new Error("Failed to request password reset.");
+      
+  const response = await forgotpasswordPost(data)
+        enqueueSnackbar(response as string, { variant: 'success' })
+      } catch (error) {
+        enqueueSnackbar(error as string, { variant: 'error' })
       }
-
-      setMessage("Password reset instructions sent to your email.");
-      setError("");
-      enqueueSnackbar(message as string, { variant: 'success' })
-    } catch (err) {
-      setError("Something went wrong. Please try again later.");
-      setMessage("");
-      enqueueSnackbar(error as string, { variant: 'error' })
-    }
-  };
- 
-  return (
+    },
+    [enqueueSnackbar]
+  )
+   return (
     <Container>
-      <Typography style={{fontSize:"35px" ,marginRight: "150px",
-  marginBottom: "15px",}}>Forgot your Password</Typography>
-        
-        <Typography style={{ 
-  marginBottom: "20px",
-  marginRight: "150px"}}
+      <Typography style={style.title}>Forgot your Password</Typography>
+        <Typography style={style.para}
           >Please enter the email address you'd like your password <br/>reset
           information sent to</Typography>
-        
-       
         <form onSubmit={handleSubmit(onSubmit)}>
-          
-            <Typography style={{marginRight:"400px"}}>Enter email address</Typography>
+          <Typography style={{marginRight:"400px"}}>Enter email address</Typography>
             <Controller
           name="email"
           control={control}
@@ -82,25 +54,18 @@ const ForgotPassword = () => {
             />
           )}
           />
-            
-          
           <Button type="submit" variant="contained"
           color="primary"
           fullWidth
           style={{
             marginBottom: '20px',
-            
-    fontSize: "20px",
+          fontSize: "20px",
           }}>Request reset link</Button>
-          {/* {message &&  <p style={{ color: "green" }}>{message}</p>}
-        {error && <p style={{ color: "red" }}>{error}</p>} */}
-    
-          <Typography style={{ textAlign:"center" ,fontSize:"20px",padding:"20px"}}>
+        <Typography style={style.link}>
             <Link to="/login">Back To Login</Link>
           </Typography>
         </form>
-        
-    </Container>
+      </Container>
   );
 };
 
---
+++ src/pages/forgotPassword/forgotpassword.style.ts
@@ -0,0 +1,10 @@
+import { CSSProperties } from "react";
+
+export const title:CSSProperties={fontSize:"35px" ,marginRight: "150px",
+  marginBottom: "15px",}
+
+  export const para:CSSProperties={ 
+    marginBottom: "20px",
+    marginRight: "150px"}
+
+    export const link:CSSProperties={ textAlign:"center" ,fontSize:"20px",padding:"20px"}
