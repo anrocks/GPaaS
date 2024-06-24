0a58e1248aef65ee2e185e63a7055ab0f742633c
anrocks <ashishnimrot1290@gmail.com>
2024-06-24 23:07:00 +0530

    New Register Desgine

--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -1,20 +1,21 @@
 import {
   Button,
-  Checkbox,
+
   Container,
-  FormControlLabel,
+
   TextField,
   Typography,
 } from '@mui/material'
-import { Link } from 'react-router-dom'
+
 import * as style from './style'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { registerPost } from '../../api/endpoints/register'
 import { dRegisterForm, RegisterForm } from '../../api/models/Register'
 import { useSnackbar } from 'notistack'
-import { useCallback, useState, useTransition } from 'react'
+import { useCallback, } from 'react'
 import { Controller, useForm } from 'react-hook-form'
 import { useTranslation } from 'react-i18next'
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 const Register = () => {
   const { t } = useTranslation()
@@ -23,7 +24,7 @@ const Register = () => {
     resolver: zodResolver(RegisterForm),
   })
 
-  const [Terms, setTerms] = useState(false)
+
 
   const onSubmit = useCallback(
     async (data: dRegisterForm) => {
@@ -37,14 +38,12 @@ const Register = () => {
     [enqueueSnackbar]
   )
 
-  const handleTerms = () => {
-    console.log('Called', Terms)
-    setTerms((prev) => !prev)
-  }
 
   return (
-    <Container maxWidth="sm">
-      <Typography style={style.title}><b>{t('register.title')}</b></Typography>
+    <Container maxWidth="sm" style={style.container}>
+      <Typography style={style.text}>Traccar
+      </Typography>
+      <Typography style={style.title}><b> <ArrowBackIcon style={{ color: "blue", fontSize: "25px", marginRight: '25px' }} />REGISTER</b></Typography>
       <form onSubmit={handleSubmit(onSubmit)}>
         <Controller
           name="name"
@@ -100,38 +99,6 @@ const Register = () => {
             />
           )}
         />
-        <Controller
-          name="repeatpassword"
-          control={control}
-          defaultValue=""
-          render={({ field, fieldState }) => (
-            <TextField
-              {...field}
-              label={t('general.repeatpassword')}
-              type="repeatPassword"
-              error={!!fieldState.error}
-              helperText={fieldState.error?.message}
-              fullWidth
-              margin="normal"
-              variant="outlined"
-              style={{ marginBottom: '20px' }}
-            />
-          )}
-        />
-        <FormControlLabel
-          control={
-            <Checkbox checked={Terms} onChange={handleTerms} color="primary" />
-          }
-          label={
-            <>
-              {t('register.iAgree')}
-              <span style={{ textDecoration: 'underline' }}>
-                {t('register.termAndCondition')}
-              </span>
-            </>
-          }
-          style={style.check}
-        />
         <Button
           type="submit"
           variant="contained"
@@ -139,36 +106,12 @@ const Register = () => {
           fullWidth
           style={{
             marginBottom: '20px',
-            width: '40%',
-            marginRight: '100px',
           }}
         >
-          {t('register.signup')}
+          REGISTER
         </Button>
-        
-        <Button
-          type="submit"
-          variant="contained"
-          color="primary"
-          fullWidth
-          style={{
-            marginBottom: '20px',
-            width: '40%',
-          }}
-        >
-         {t('general.cancel')} 
-        </Button>
-        <Typography
-          style={{
-            textAlign: 'center',
-            marginTop: '20px',
-          }}
-        >
-          {t('register.account')}
-          <Link to="/login">
-            <b>{t('register.login')}</b>
-          </Link>
-        </Typography>
+
+
       </form>
     </Container>
   )
--- src/pages/register/style.ts
+++ src/pages/register/style.ts
@@ -1,10 +1,25 @@
 import { CSSProperties } from 'react'
 export const title: CSSProperties = {
-  textAlign: 'center',
-  marginBottom: '20px',
+  color: 'blue',
+  fontSize: '25px',
+  marginRight: '150px',
 }
-export const check: CSSProperties = {
-  marginBottom: '20px',
-  display: 'flex',
-  justifyContent: 'left',
+
+export const text: CSSProperties = {
+  position: 'absolute',
+  left: '20px',
+  bottom: '0px',
+  backgroundColor: '#0C3B9C',
+  color: 'white',
+  height: '100%',
+  width: '500px',
+  transition: 'width 2s',
+  fontSize: '45px',
+  lineHeight: '750px',
+}
+export const container: CSSProperties = {
+  maxWidth: '400px',
+  marginLeft: '500px',
+  padding: '10px',
+  borderRadius: '8px',
 }
